f5367e80b8c4a7d3766cd067278e16e6627b37a1
Barthmosz <barthmosz@gmail.com>
2023-06-17 02:10:01 -0300

    feat: implement push method of stack

---
+++ data-structure/stack.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node* next;
+};
+
+class Stack {
+    public:
+        Node* top;
+        Stack() { top = NULL; }
+
+        void push(int element) {
+            Node* node = new Node;
+            if (!node) {
+                cout << "\nHeap Overflow";
+                exit(1);
+            }
+            node->data = element;
+            node->next = top;
+            top = node;
+        }
+
+        void display() {
+            Node* temp;
+            if (top == NULL) {
+                cout << "\nStack Underflow";
+                exit(1);
+            } else {
+                temp = top;
+                while (temp != NULL) {
+                    cout << temp->data << "-> ";
+                    temp = temp->next;
+                }
+            }
+        }
+};
+
+int main() {
+    Stack stack;
+    return 0;
+}
