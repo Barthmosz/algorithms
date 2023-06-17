f42aeaa57451864240caaa9828b7d0f64083484f
Barthmosz <barthmosz@gmail.com>
2023-06-17 20:15:21 -0300

    refactor: add a hpp file to Stack

---
+++ sorting-algorithms/stack/Stack.hpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+enum ExitCodes {
+    STACK_UNDERFLOW = 1,
+    HEAP_OVERFLOW = 2
+};
+
+class Node {
+    public:
+        int data;
+        Node* next;
+};
+
+class Stack {
+    public:
+        Node* top;
+        Stack();
+        
+        void push(int element);
+        void pop();
+        void peek();
+        bool isEmpty();
+        void display();
+};
