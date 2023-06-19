d25ccc14057944d159adf9ed6252c6571ac42f06
Barthmosz <barthmosz@gmail.com>
2023-06-18 21:00:45 -0300

    feat: implement Queue constructor and deconstructor

---
+++ sorting-algorithms/queue/Queue.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Queue.hpp"
+
+Queue::Queue(unsigned int capacity) {
+    capacity = capacity;
+    queue = new int[capacity];
+    front = count = 0;
+    rear = capacity - 1;
+}
+
+Queue::~Queue() {
+    delete[] queue;
+}
